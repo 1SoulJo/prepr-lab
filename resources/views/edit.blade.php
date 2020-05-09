eff1c64011319365d2368d42b43bf8e3ca0dc4d3
root <root@Hansol.localdomain>
2020-05-09 17:19:15 -0400

    Fix css

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,19 +1,17 @@
 @extends('layouts.app')
 
-//this will add laravel’s default navbar to your page
-
 @section('content')
 
 <div class="container">
-	<h1>Edit the Task</h1>
+	<h1>Edit the Lab</h1>
 
-    <form method="POST" action="/task/{{ $task->id }}">
+    <form method="POST" action="/lab/{{ $lab->id }}">
         <div class="form-group">
-            <textarea name="name" class="form-control">{{$task->description }}</textarea>	
+            <textarea name="name" class="form-control">{{$lab->name }}</textarea>	
         </div>
 
         <div class="form-group">
-            <button type="submit" name="update" class="btn btn-primary">Update task</button>
+            <button type="submit" name="update" class="btn btn-primary">Update lab</button>
         </div>
     {{ csrf_field() }}
     </form>
